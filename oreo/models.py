04edde2dd266870a391e8c36ccb3e80bbd7a9618
caiseyann <caiseyann4@gmail.com>
2020-10-20 13:39:46 +0300

    created class profile

--- oreo/models.py
+++ oreo/models.py
@@ -4,3 +4,9 @@ from tinymce.models import HTMLField
 import datetime as dt
 
 # Create your models here.
+class Profile(models.Model):
+    bio = HTMLFields()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    avatar = models.ImageField(upload_to='images/', blank=True)
+
+    
