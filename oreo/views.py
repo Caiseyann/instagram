181b6ded4cab8ead8525b2a9ab48e427cc60b9f7
caiseyann <caiseyann4@gmail.com>
2020-10-20 13:57:41 +0300

    importing modules for views.py

--- oreo/views.py
+++ oreo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import *
+from .models import *
+from .forms import *
+from .email import *
+import datetime as dt
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.shortcuts import render,redirect
 
 # Create your views here.
