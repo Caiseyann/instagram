215971ccc1e00ad694d4d7f6ac7e99545bdc14e6
caiseyann <caiseyann4@gmail.com>
2020-10-20 13:44:19 +0300

    defining functions

--- oreo/models.py
+++ oreo/models.py
@@ -9,4 +9,15 @@ class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
     avatar = models.ImageField(upload_to='images/', blank=True)
 
-    
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def get_profile(cls): 
+        profile = Profile.objects.get(user = id)
+        return profile 
+
+    @classmethod
+    def get_profile(cls, id):
+        profile = Profile.objects.all()
+        return profile 
